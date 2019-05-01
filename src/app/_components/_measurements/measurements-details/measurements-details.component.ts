faf7563074e3e44278c100ca4e0e5029d30a624a
lukasz <lkszblaszczyk@gmail.com>
2019-05-01 16:09:21 +0200

    measurement details

--- src/app/_components/_measurements/measurements-details/measurements-details.component.ts
+++ src/app/_components/_measurements/measurements-details/measurements-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {MeasurementService} from '../../../_services/measurement.service';
+import {Measurement} from '../../../_models/measurement';
+import {ActivatedRoute} from '@angular/router';
+import {StateAcceptedRequest} from '../../../_models/_requests/stateAcceptedRequest';
+import {SnackBarGenerator} from '../../../_helpers/snackBarGenerator';
 
 @Component({
   selector: 'app-measurements-details',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeasurementsDetailsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  measurementId: string;
+  measurement: Measurement;
+  constructor(private measurementsService: MeasurementService,
+              private route: ActivatedRoute,
+              private snackBar: SnackBarGenerator) {
+    if (this.route.snapshot.params.id) {
+      this.measurementId = this.route.snapshot.params.id;
+    }
   }
 
+  ngOnInit() {
+    this.getMeasurement();
+  }
+
+  getMeasurement() {
+    this.measurementsService.getMeasurement(this.measurementId).subscribe((measurement: Measurement) => {
+      if (measurement) {
+        this.measurement = measurement;
+        console.log('measurement: ', this.measurement);
+      }
+    });
+  }
+
+  acceptMeasurement() {
+    let measurementStatus = new StateAcceptedRequest();
+    measurementStatus.accepted = true;
+    this.measurementsService.changeMeasurementStatus(this.measurement.id, measurementStatus).subscribe(res => {
+      this.snackBar.openSnackBar('Zaakceptowano odczyt', true);
+      this.getMeasurement();
+    });
+  }
+
+  denyMeasurement() {
+    let measurementStatus = new StateAcceptedRequest();
+    measurementStatus.accepted = false;
+    this.measurementsService.changeMeasurementStatus(this.measurement.id, measurementStatus).subscribe(res => {
+      this.snackBar.openSnackBar('Odrzucono odczyt', true);
+      this.getMeasurement();
+    });
+  }
 }
--- src/app/_models/measurement.ts
+++ src/app/_models/measurement.ts
@@ -1,11 +1,14 @@
+import {Premise} from './premise';
+
 export class Measurement {
   id: number;
   electricity: number;
   hotWater: number;
   coldWater: number;
   heating: number;
-  isChecked: boolean;
-  isAccepted: boolean;
+  accepted: boolean;
+  checked: boolean;
   month: number;
   year: number;
+  premise: Premise;
 }
